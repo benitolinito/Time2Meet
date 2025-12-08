ae9c64b062838e44cd7b8ca487ea01bbb9a5a606
Ben Lin <blin97198@gmail.com>
2025-12-08 00:35:50 -0500

    keys

--- app/src/firebase.ts
+++ app/src/firebase.ts
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDLupdQJsAnw3rRB2Zwu8ieX5icPCSQin0",
-  authDomain: "time2meet-123.firebaseapp.com",
-  projectId: "time2meet-123",
-  storageBucket: "time2meet-123.firebasestorage.app",
-  messagingSenderId: "450651838680",
-  appId: "1:450651838680:web:03914d534a44c22c2c9fad",
-  measurementId: "G-DW5J7991DW"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
